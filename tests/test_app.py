8531ff5ad20528bd9f613bebe8fd2517c4dc07b2
ihabfaour <iaf08@mail.aub.edu>
2024-11-07 16:31:56 +0200

    tried pytest locally and it worked

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,16 @@
+import sys
+import os
+
+# Ensure the root directory is in the PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
-from app import greet
+from app1 import greet
 
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World"), "Hello, World from Ihab Faour!")
         
 if __name__ == "__main__":
     unittest.main()
